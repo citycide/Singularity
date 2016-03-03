49438360603cd16e720ae10cebc0afe47fae64f9
citycide <thecitycide@gmail.com>
2016-03-03 15:47:19 -0600

    Update client.js

--- js/client.js
+++ js/client.js
@@ -2,31 +2,29 @@ var gui = require('nw.gui');
 var socket = io();
 
 function sendFollowTest() {
-	$("#btnTestFollower").click(function() {
-		var user = $("#testFollowerUser").val();
-		socket.emit('newFollower', user);
-		return false;
-	});
+    $("#btnTestFollower").click(function() {
+        var user = $("#testFollowerUser").val();
+        socket.emit('newFollower', user);
+        console.log('Sent follower test with name: ' + user);
+        return false;
+    });
 }
 
+var winAlerts;
 $('a#openAlerts').click(function() {
-	openAlerts();
+    winAlerts = gui.Window.open('public/index.html', {
+        position: 'center',
+        width: 1280,
+        height: 720
+    });
 });
 
-function openAlerts() {
-  var winAlerts = gui.Window.open('public/index.html', {
-    position: 'center',
-    width: 1280,
-    height: 720
-  });
+winAlerts.on('closed', function() {
+  winAlerts = null;
+});
 
-	winAlerts.on('closed', function() {
-	  winAlerts = null;
-	});
-
-	gui.Window.get().on('close', function() {
-	  this.hide();
-	  if (winAlerts != null) winAlerts.close(true);
-	  this.close(true);
-	});
-}
+gui.Window.get().on('close', function() {
+  this.hide();
+  if (winAlerts != null) winAlerts.close(true);
+  this.close(true);
+});
