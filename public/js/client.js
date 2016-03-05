6ee5e911e21806fc699a951fc15dd87c01d66052
citycide <thecitycide@gmail.com>
2016-03-04 23:00:36 -0600

    Updates

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,5 @@
-var mainwin,
-    gui,
-    socket;
-
 $(document).ready( function() {
-    gui = require('nw.gui');
-    socket = io.connect('http://localhost:2882');
+    var socket = io.connect('http://localhost:2882');
 
     mainwin = gui.Window.get();
     mainwin.on("close", function() {
@@ -37,7 +32,7 @@ $(document).ready( function() {
 
     $("#btnTestFollower").click(function() {
         var user = $("#testFollowerUser").val();
-        io.emit('newFollower', user);
+        socket.emit('newFollower', user);
         console.log('TEST: Sent follower test with name: ' + user + '.');
         return false;
     });
@@ -45,7 +40,7 @@ $(document).ready( function() {
     $("#btnTestHost").click(function() {
         var user = $("#testHostUser").val();
         var viewers = parseInt($("#testHostViewers").val());
-        io.emit('newHoster', [user, viewers]);
+        socket.emit('newHoster', [user, viewers]);
         console.log('TEST: Sent host test with: ' + user + ' for ' + viewers + ' viewers.');
         return false;
     });
@@ -54,10 +49,10 @@ $(document).ready( function() {
         var user = $("#testSubUser").val();
         var months = parseInt($("#testSubMonths").val());
         if (months === null || months === undefined || months === 0 || isNaN(months)) {
-            io.emit('newSubscriber', user);
+            socket.emit('newSubscriber', user);
             console.log('TEST: Sent new subscriber test with name: ' + user + '.');
         } else {
-            io.emit('newResub', [user, months]);
+            socket.emit('newResub', [user, months]);
             console.log('TEST: Sent resubscriber test with: ' + user + ' for ' + months + ' months.');
         }
         return false;
@@ -67,7 +62,7 @@ $(document).ready( function() {
         var user = $("#testDonationUser").val();
         var amount = parseInt($("#testDonationAmt").val());
         var message = $("#testDonationMsg").val();
-        io.emit('newDonation', [user, amount, message]);
+        socket.emit('newDonation', [user, amount, message]);
         if (message === "" || message === null || message === undefined) {
             console.log('TEST: Sent new donation test from: ' + user + ' for '
                 + '$' + amount + '.');
