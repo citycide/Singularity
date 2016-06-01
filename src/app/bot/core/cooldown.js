7a8d3b9ed3dc259c4db77e90a454d4cb7338dd08
citycide <thecitycide@gmail.com>
2016-06-01 01:57:59 -0500

    {refactor} bot cooldown module
    cooldown.js is now hugely simplified and heavily documented. Lots of
    comments.

--- src/app/bot/core/cooldown.js
+++ src/app/bot/core/cooldown.js
@@ -1,27 +1,30 @@
 const cooldown = {
+    // cooldowns['command-name'] = { name, until, scope }
     cooldowns: [],
     set(cmd, user) {
+        // if this command has no default specified, use the bot default
+        const time = core.command.getCooldown(cmd) || this.getDefault();
         this.cooldowns.push({
             name: cmd,
-            until: Date.now() + (core.command.getCooldown(cmd) * 1000),
-            scope: (core.settings.get('globalCooldown')) ? true : user
+            until: Date.now() + (time * 1000),
+            // if globalCooldown is set to true or no user was provided
+            scope: (core.settings.get('globalCooldown') || !user)
+                // a value of false for cooldown scope means global, ie. all users
+                ? false
+                // handle the case where no user is provided
+                : user || false
         });
     },
     clear(cmd, user) {
-        if (arguments.length < 2) {
-            for (let [index, command] of this.cooldowns.entries()) {
-                if (command.name === cmd && command.scope === true) {
-                    this.cooldowns.splice(index, 1);
-                    return false;
-                }
-            }
+        const index = this.getIndex(cmd, user);
+
+        // check that the item was actually in the array, remove if it was
+        if (index >= 0) {
+            // returns true if exactly 1 item was removed, otherwise false
+            return (this.cooldowns.splice(index, 1).length === 1);
         } else {
-            for (let [index, command] of this.cooldowns.entries()) {
-                if (command.name === cmd && command.scope === user) {
-                    this.cooldowns.splice(index, 1);
-                    return false;
-                }
-            }
+            // if the item was not in the array, do nothing
+            return false;
         }
     },
     clearAll() {
@@ -31,30 +34,38 @@ const cooldown = {
         return core.settings.get('defaultCooldown');
     },
     isActive(cmd, user) {
-        const hasCooldown = core.command.getCooldown(cmd);
-        if (core.settings.get('globalCooldown') && !hasCooldown) {
-            for (let [index, command] of this.cooldowns.entries()) {
-                if (command.name === cmd && command.scope === true) {
-                    const timeLeft = command.until - Date.now();
-                    if (timeLeft > 0) {
-                        return parseInt(timeLeft / 1000);
-                    } else {
-                        this.cooldowns.splice(index, 1);
-                        return false;
-                    }
+        // see above for comments about cooldown scope
+        const scope = (core.settings.get('globalCooldown') || !user)
+            ? false
+            : user || false;
+
+        for (let [index, command] of this.cooldowns.entries()) {
+            // if we matched a command name & scope combination
+            if (command.name === cmd && command.scope === scope) {
+                const timeLeft = command.until - Date.now();
+
+                if (timeLeft > 0) {
+                    // return the number of seconds left if > 0
+                    return parseInt(timeLeft / 1000);
+                } else {
+                    // remove the cooldown if the time has reached 0
+                    // returns false if exactly 1 item was removed, otherwise true
+                    return !(this.cooldowns.splice(index, 1).length === 1);
                 }
             }
-        } else {
-            for (let [index, command] of this.cooldowns.entries()) {
-                if (command.name === cmd && command.scope === user) {
-                    const timeLeft = command.until - Date.now();
-                    if (timeLeft > 0) {
-                        return parseInt(timeLeft / 1000);
-                    } else {
-                        this.cooldowns.splice(index, 1);
-                        return false;
-                    }
-                }
+        }
+    },
+    getIndex(cmd, user) {
+        // see above for comments about cooldown scope
+        const scope = (core.settings.get('globalCooldown') || !user)
+            ? false
+            : user || false;
+
+        for (let [index, command] of this.cooldowns.entries()) {
+            // if we matched a command name & scope combination
+            if (command.name === cmd && command.scope === scope) {
+                // return the position in the array
+                return index;
             }
         }
     }
