dfb20a3e33ef5ee03897ae44b81898bdd6f16646
citycide <thecitycide@gmail.com>
2016-06-09 13:59:09 -0500

    {refactor} Move getPermLevel into groups component
    Renamed to getUserGroup and on the API as $.users.getGroup()
    Uses $.util for a few things to test it out.

--- src/app/bot/components/groups.js
+++ src/app/bot/components/groups.js
@@ -1,11 +1,34 @@
 const groups = {
-    // ... group stuff
+    getUserGroup(user) {
+        // 'user' parameter should always be an object
+        // minimum requirements:
+        // user = { 'display-name': 'name' };
+        
+        const username = user['display-name'];
+        const userType = user['user-type'];
+        if (!username) return;
+        
+        let defaultGroupID = 5;
+
+        if (userType === 'mod') defaultGroupID = 1;
+        if ($.users.isAdmin(username)) defaultGroupID = 0;
+    
+        const _groupID = $.util.num.validate($.data.get('users', 'permission', { name: username }));
+        if (!$.util.val.isNullLike(_groupID) && _groupID >= 0 && !$.users.isAdmin(username)) {
+            return _groupID;
+        } else {
+            Logger.trace(`getUserGroup:: assigning default group to ${username} (level ${defaultGroupID})`);
+            return defaultGroupID;
+        }
+    }
 };
 
 /**
  * Add methods to the global core object
  **/
 $.on('bot:ready', () => {
+    $.users.getGroup = groups.getUserGroup;
+    
     if ($.settings.get('groups', 'initial') === 'initial') {
         $.data.set('groups', { name: 'admin', level: 0, bonus: 0 });
         $.data.set('groups', { name: 'moderator', level: 1, bonus: 0 });
