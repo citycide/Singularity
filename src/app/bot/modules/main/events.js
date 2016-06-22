9333531e22dc3a9e3675c348ec36ea0b00d651f0
citycide <thecitycide@gmail.com>
2016-06-22 15:09:54 -0500

    {lang} alerts module uses lang file

--- src/app/bot/modules/main/events.js
+++ src/app/bot/modules/main/events.js
@@ -4,13 +4,15 @@ module.exports.alerts = (event) => {
     if (event.subcommand === 'follow') {
         if (param1 === 'enable') {
             $.settings.set('followAlerts', true);
-            $.say(event.sender, `Follow alerts enabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-follows:enabled:success'));
         } else if (param1 === 'disable') {
             $.settings.set('followAlerts', false);
-            $.say(event.sender, `Follow alerts disabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-follows:disabled:success'));
         } else {
-            const status = $.settings.get('followAlerts', true) ? 'enabled' : 'disabled';
-            $.say(event.sender, `Usage: !alerts follow [enable | disable] (currently ${status})`);
+            const status = $.settings.get('followAlerts', true)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled');
+            $.say(event.sender, weave.get('bot:modules:events:alerts:follow:usage', status));
         }
 
         return;
@@ -19,13 +21,15 @@ module.exports.alerts = (event) => {
     if (event.subcommand === 'host') {
         if (param1 === 'enable') {
             $.settings.set('hostAlerts', true);
-            $.say(event.sender, `Host alerts enabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-hosts:enabled:success'));
         } else if (param1 === 'disable') {
             $.settings.set('hostAlerts', false);
-            $.say(event.sender, `Host alerts enabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-hosts:disabled:success'));
         } else {
-            const status = $.settings.get('hostAlerts', true) ? 'enabled' : 'disabled';
-            $.say(event.sender, `Usage: !alerts host [enable | disable] (currently ${status})`);
+            const status = $.settings.get('hostAlerts', true)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled');
+            $.say(event.sender, weave.get('bot:modules:events:alerts:host:usage', status));
         }
 
         return;
@@ -34,13 +38,15 @@ module.exports.alerts = (event) => {
     if (event.subcommand === 'sub') {
         if (param1 === 'enable') {
             $.settings.set('subAlerts', true);
-            $.say(event.sender, `Subscriber alerts enabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-subs:enabled:success'));
         } else if (param1 === 'disable') {
             $.settings.set('subAlerts', false);
-            $.say(event.sender, `Subscriber alerts disabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-subs:disabled:success'));
         } else {
-            const status = $.settings.get('subAlerts', false) ? 'enabled' : 'disabled';
-            $.say(event.sender, `Usage: !alerts sub [enable | disable] (currently ${status})`);
+            const status = $.settings.get('subAlerts', false)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled');
+            $.say(event.sender, weave.get('bot:modules:events:alerts:sub:usage', status));
         }
 
         return;
@@ -49,13 +55,15 @@ module.exports.alerts = (event) => {
     if (event.subcommand === 'tip') {
         if (param1 === 'enable') {
             $.settings.set('tipAlerts', true);
-            $.say(event.sender, `Tip alerts enabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-tips:enabled:success'));
         } else if (param1 === 'disable') {
             $.settings.set('tipAlerts', false);
-            $.say(event.sender, `Tip alerts disabled.`);
+            $.say(event.sender, weave.get('bot:settings:alerts-tips:disabled:success'));
         } else {
-            const status = $.settings.get('tipAlerts', false) ? 'enabled' : 'disabled';
-            $.say(event.sender, `Usage: !alerts tip [enable | disable] (currently ${status})`);
+            const status = $.settings.get('tipAlerts', false)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled');
+            $.say(event.sender, weave.get('bot:modules:events:alerts:tip:usage', status));
         }
 
         return;
@@ -63,19 +71,26 @@ module.exports.alerts = (event) => {
 
     if (event.subcommand === 'settings') {
         const cfg = [
-            $.settings.get('followAlerts', true) ? 'enabled' : 'disabled',
-            $.settings.get('hostAlerts', true) ? 'enabled' : 'disabled',
-            $.settings.get('subAlerts', false) ? 'enabled' : 'disabled',
-            $.settings.get('tipAlerts', false) ? 'enabled' : 'disabled'
+            $.settings.get('followAlerts', true)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled'),
+            $.settings.get('hostAlerts', true)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled'),
+            $.settings.get('subAlerts', false)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled'),
+            $.settings.get('tipAlerts', false)
+                ? weave.get('common-words:enabled')
+                : weave.get('common-words:disabled')
         ];
 
-        $.say(event.sender,
-            `Follows: ${cfg[0]}, Hosts: ${cfg[1]}, Subs: ${cfg[2]}, Tips: ${cfg[3]}`);
+        $.say(event.sender, weave.get('bot:modules:events:alerts:settings', ...cfg);
 
         return;
     }
 
-    $.say(event.sender, `Usage: !alerts [follow | host | sub | tip]`);
+    $.say(event.sender, weave.get('bot:modules:events:alerts:usage');
 };
 
 // Keep an array of events to prevent duplicates
@@ -85,19 +100,33 @@ Transit.on('alert:follow', (data) => {
     if ($.settings.get('followAlerts', true)) {
         if (!events.includes(`${data.display_name}:follow`)) {
             events.push(`${data.display_name}:follow`);
-            return $.shout(`Thanks for the follow, ${data.display_name}!`);
+            const reward = $.settings.get('followReward', 50);
+
+            if (reward > 0) {
+                $.shout(weave.get('bot:settings:alerts-follows:response-reward',
+                    data.display_name, $.points.str(reward));
+            } else {
+                $.shout(weave.get('bot:settings:alerts-follows:response-no-reward',
+                    data.display_name))
+            }
         }
     }
 });
 
 Transit.on('alert:host', (data) => {
-    // eslint-disable-next-line
-    const { name: display_name, viewers } = data;
     if ($.settings.get('hostAlerts', true)) {
-        if (!events.includes(`${name}:host`)) {
+        if (!events.includes(`${data.display_name}:host`)) {
             // Only consider hosts duplicates if the viewer count is the same
-            events.push(`${name}:host:${viewers}`);
-            return $.shout(`${name} fired up the host machine for ${viewers} viewers. Thanks!`);
+            events.push(`${data.display_name}:host:${data.viewers}`);
+            const reward = $.settings.get('hostReward', 50);
+
+            if (reward > 0) {
+                $.shout(weave.get('bot:settings:alerts-hosts:response-reward',
+                    data.display_name, data.viewers, $.points.str(reward));
+            } else {
+                $.shout(weave.get('bot:settings:alerts-hosts:response-no-reward',
+                    data.display_name, data.viewers));
+            }
         }
     }
 });
@@ -106,14 +135,30 @@ Transit.on('alert:subscriber', (data) => {
     if ($.settings.get('subAlerts', false)) {
         if (!events.includes(`${data.display_name}:sub`)) {
             events.push(`${data.display_name}:sub`);
+            const reward = $.settings.get('subReward', 50);
+            let response = '';
 
             if (data.hasOwnProperty('months')) {
                 // Event is a resub
-                return $.shout(`${data.display_name} has been subbed for ${data.months} months!`);
+                if (reward > 0) {
+                    response = weave.get('bot:settings:alerts-resubs:response-reward',
+                        data.display_name, data.months, $.points.str(reward));
+                } else {
+                    response = weave.get('bot:settings:alerts-resubs:response-no-reward',
+                        data.display_name, data.months);
+                }
             } else {
                 // Event is a new subscription
-                return $.shout(`Thanks for subscribing, ${data.display_name}!`);
+                if (reward > 0) {
+                    response = weave.get('bot:settings:alerts-subs:response-reward',
+                        data.display_name, $.points.str(reward));
+                } else {
+                    response = weave.get('bot:settings:alerts-subs:response-no-reward',
+                        data.display_name);
+                }
             }
+
+            $.shout(response);
         }
     }
 });
@@ -121,7 +166,15 @@ Transit.on('alert:subscriber', (data) => {
 Transit.on('alert:tip', (data) => {
     if ($.settings.get('tipAlerts', false)) {
         // Tip alerts are probably not duplicates, so don't check
-        return $.shout(`${data.name} tipped ${data.amount}. Thank you! PogChamp`);
+        const reward = $.settings.get('tipReward', 50);
+
+        if (reward > 0) {
+            $.shout(weave.get('bot:settings:alerts-tips:response-reward',
+                data.name, data.amount, $.points.str(reward));
+        } else {
+            $.shout(weave.get('bot:settings:alerts-tips:response-no-reward',
+                data.name, data.amount));
+        }
     }
 });
 
