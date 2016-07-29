39d42ac26c58654c2c8e8e0a717d0bfcc7df890c
citycide <thecitycide@gmail.com>
2016-07-28 21:11:44 -0500

    {refactor} make Tock compatible with native setTimeout
    Tock's unique id for timeouts / intervals is now optional, making the API backwards compatible with setTimeout. Also added a ton of inline docs for it.

--- src/common/utils/Tock.js
+++ src/common/utils/Tock.js
@@ -1,48 +1,148 @@
+/**
+ * @class Tock
+ */
 export default class Tock {
+  /**
+   * Create a new `Tock` instance
+   */
   constructor () {
     this.timers = new Map()
     this.intervals = new Map()
   }
 
-  setTimeout (uid, fn, time, ...args) {
-    if (arguments.length < 3) {
+  /**
+   * Has the same signature as native `window.setTimeout`
+   * but with the addition of an optional first parameter
+   * that is used as a unique identifier.
+   *
+   * If `uid` is set, calling this method again with the
+   * same `uid` will clear the previous timeout before
+   * setting the new one.
+   *
+   * @memberof Tock
+   * @instance
+   * @param {*} [uid] - unique identifier, anything but a function
+   * @param {function} fn
+   * @param {number} [time=1] - defaults to 1ms
+   * @param {...*} args - arguments to be passed to `fn`
+   *
+   * @returns {number}
+   */
+  setTimeout (uid, fn, time = 1, ...args) {
+    if (typeof uid === 'function') {
+      time = parseInt(fn) || 1
+      fn = uid
+      uid = undefined
+    } else if (arguments.length < 3) {
       throw new Error('Tock#setTimeout :: wrong number of arguments.')
     }
-    
+
     if (typeof fn !== 'function') {
-      throw new Error('Tock#setTimeout :: second argument must be a function.')
+      throw new Error(`Tock#setTimeout :: 'fn' parameter must be a function.`)
     }
 
-    if (this.timers.has(uid)) this.clearTimeout(uid)
-    const id = setTimeout(fn, time, ...args)
-    this.timers.set(uid, id)
-    return this.timers.get(uid)
+    if (!uid) {
+      // just return a native setTimeout ID
+      return setTimeout(fn, time, ...args)
+    } else {
+      // clear any existing timeout with this ID
+      this.clearTimeout(uid)
+
+      const id = setTimeout(this._wrapper.bind(this), time, fn, uid, ...args)
+      this.timers.set(uid, id)
+      return this.timers.get(uid)
+    }
   }
 
+  /**
+   * Clear a timeout and remove its reference
+   * [Tock's setTimeout method]{@link Tock#setTimeout} must
+   * previously have been called with the `uid` parameter
+   *
+   * @memberof Tock
+   * @instance
+   * @param {*} uid
+   */
   clearTimeout (uid) {
     if (!this.timers.has(uid)) return
-    const id = this.timers.get(uid)
-    return clearTimeout(id)
+
+    clearTimeout(this.timers.get(uid))
+    this.timers.delete(uid)
   }
 
-  setInterval (uid, fn, interval, ...args) {
-    if (arguments.length < 3) {
+  /**
+   * Has the same signature as native `window.setInterval`
+   * but with the addition of an optional first parameter
+   * that is used as a unique identifier.
+   *
+   * If `uid` is set, calling this method again with the
+   * same `uid` will clear the previous interval before
+   * setting the new one.
+   *
+   * @memberof Tock
+   * @instance
+   * @param {*} [uid] - unique identifier, anything but a function
+   * @param {function} fn
+   * @param {number} [interval=1000] - defaults to 1s
+   * @param {...*} args - arguments to be passed to `fn`
+   *
+   * @returns {number}
+   */
+  setInterval (uid, fn, interval = 1000, ...args) {
+    if (typeof uid === 'function') {
+      interval = parseInt(fn) || 1
+      fn = uid
+      uid = undefined
+    } else if (arguments.length < 3) {
       throw new Error('Tock#setInterval :: wrong number of arguments.')
     }
-    
+
     if (typeof fn !== 'function') {
-      throw new Error('Tock#setInterval :: second argument must be a function.')
+      throw new Error(`Tock#setInterval :: 'fn' parameter must be a function.`)
     }
 
-    if (this.intervals.has(uid)) this.clearInterval(uid)
-    const id = setInterval(fn, interval, ...args)
-    this.intervals.set(uid, id)
-    return this.intervals.get(uid)
+    if (!uid) {
+      // just return a native setInterval ID
+      return setInterval(fn, interval, ...args)
+    } else {
+      // clear any existing interval with this ID
+      this.clearInterval(uid)
+
+      const id = setInterval(fn, interval, ...args)
+      this.intervals.set(uid, id)
+      return this.intervals.get(uid)
+    }
   }
 
+  /**
+   * Clear an interval and remove its reference
+   * [Tock's setInterval method]{@link Tock#setInterval} must
+   * previously have been called with the `uid` parameter
+   *
+   * @memberof Tock
+   * @instance
+   * @param {*} uid
+   */
   clearInterval (uid) {
     if (!this.intervals.has(uid)) return
-    const id = this.intervals.get(uid)
-    return clearInterval(id)
+
+    clearInterval(this.intervals.get(uid))
+    this.intervals.delete(uid)
+  }
+
+  /**
+   * A wrapper function around the user's callback
+   * When setTimeout calls the wrapper it will call
+   * the user's callback function, then remove its
+   * reference from the timer Map.
+   *
+   * @param {function} fn
+   * @param {*} uid
+   * @param {...*} args - arguments to pass to `fn`
+   * @private
+   */
+  _wrapper (fn, uid, ...args) {
+    fn(...args)
+    this.timers.delete(uid)
   }
 }
