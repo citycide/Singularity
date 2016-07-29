15e0189093e23a909e3bc34bc20616ab70bc238f
citycide <thecitycide@gmail.com>
2016-07-28 21:20:56 -0500

    {refactor} gulp build process
    Remove gulp-newer ( it's great but unusable along with a cleaning process, which is kind of necessary )
    
    Add `del` module for build directory cleaning

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,72 +1,77 @@
 import gulp from 'gulp'
+import del from 'del'
 import babel from 'gulp-babel'
-import newer from 'gulp-newer'
 import vueify from 'gulp-vueify'
 import sass from 'gulp-sass'
 import pug from 'gulp-pug'
 import importer from 'node-sass-module-importer'
 
-gulp.task('main', () => {
-  gulp.src('./src/main/**/*.js')
-    .pipe(newer('./build/main'))
+gulp.task('main', ['clean:main'], () => {
+  return gulp.src('./src/main/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./build/main'))
 })
 
 gulp.task('renderer', [
+  'clean:renderer',
   'renderer:js',
   'renderer:vue',
   'renderer:pug',
   'renderer:sass',
-  'renderer:assets'
+  'renderer:assets',
+  'renderer:vendor'
 ])
 
 gulp.task('renderer:js', () => {
-  gulp.src('./src/renderer/**/*.js')
-    .pipe(newer('./build/renderer'))
+  return gulp.src(['./src/renderer/**/*.js', '!./src/renderer/**/vendor/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('./build/renderer'))
 })
 
 gulp.task('renderer:vue', () => {
-  gulp.src('./src/renderer/**/*.vue')
-    .pipe(newer('./build/renderer'))
+  return gulp.src('./src/renderer/**/*.vue')
     .pipe(vueify())
     .pipe(gulp.dest('./build/renderer'))
 })
 
 gulp.task('renderer:pug', () => {
-  gulp.src('./src/renderer/**/*.pug')
-    .pipe(newer('./build/renderer'))
+  return gulp.src('./src/renderer/**/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./build/renderer'))
 })
 
 gulp.task('renderer:sass', () => {
-  gulp.src('./src/renderer/**/*.scss')
-    .pipe(newer('./build/renderer'))
+  return gulp.src('./src/renderer/**/*.scss')
     .pipe(sass({ importer }))
     .pipe(gulp.dest('./build/renderer'))
 })
 
 // Temporary task to simply copy image files
 gulp.task('renderer:assets', () => {
-  gulp.src('./src/renderer/**/*.png')
-    .pipe(newer('./build/renderer'))
+  return gulp.src('./src/renderer/**/*.png')
     .pipe(gulp.dest('./build/renderer'))
 })
 
-gulp.task('common', () => {
-  gulp.src('./src/common/**/*.js')
-    .pipe(newer('./build/common'))
+gulp.task('renderer:vendor', () => {
+  return gulp.src('./src/renderer/**/vendor/**/*.js')
+    .pipe(gulp.dest('./build/renderer'))
+})
+
+gulp.task('common', ['clean:common'], () => {
+  return gulp.src('./src/common/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./build/common'))
 })
 
+gulp.task('clean:all', () => del('./build'))
+gulp.task('clean:main', () => del('./build/main'))
+gulp.task('clean:renderer', () => del('./build/renderer'))
+gulp.task('clean:common', () => del('./build/common'))
+
 gulp.task('watch', ['build'], () => {
-  gulp.watch('./src/main/**/*.js', ['main'])
-  gulp.watch('./src/renderer/**/*.{js,vue,pug,scss}', ['renderer'])
-  gulp.watch('./src/common/**/*.{js,vue,pug}', ['common'])
+  gulp.watch('./src/main/**/*.js', ['clean:main', 'main'])
+  gulp.watch('./src/renderer/**/*.{js,vue,pug,scss}', ['clean:renderer', 'renderer'])
+  gulp.watch('./src/common/**/*.{js,vue,pug}', ['clean:common', 'common'])
 })
 
 gulp.task('build', ['main', 'renderer', 'common'])
