94c4273e93050e66a512f2cd35febda68a83f3cc
citycide <thecitycide@gmail.com>
2016-09-13 03:27:45 -0500

    {refactor} use gulp v4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,77 +1,96 @@
-import gulp from 'gulp'
 import del from 'del'
+import gulp from 'gulp'
 import babel from 'gulp-babel'
 import vueify from 'gulp-vueify'
 import sass from 'gulp-sass'
 import pug from 'gulp-pug'
+import jetpack from 'fs-jetpack'
 import importer from 'node-sass-module-importer'
 
-gulp.task('main', ['clean:main'], () => {
+const babelVue = () => jetpack.read('./.babelrc')
+
+function cleanAll () { return del('./build') }
+function cleanMain () { return del('./build/main') }
+function cleanCommon () { return del('./build/common') }
+function cleanRenderer () { return del('./build/renderer') }
+
+const buildMain = gulp.parallel(mainJS, mainServer)
+
+function mainJS () {
   return gulp.src('./src/main/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./build/main'))
-})
+}
 
-gulp.task('renderer', [
-  'clean:renderer',
-  'renderer:js',
-  'renderer:vue',
-  'renderer:pug',
-  'renderer:sass',
-  'renderer:assets',
-  'renderer:vendor'
-])
+function mainServer () {
+  return gulp.src('./src/main/components/server/public/**/*.html')
+    .pipe(gulp.dest('./build/main/components/server/public'))
+}
 
-gulp.task('renderer:js', () => {
+const buildRenderer = gulp.parallel(
+  rendererJS,
+  rendererVue,
+  rendererPug,
+  rendererSass,
+  rendererAssets,
+  rendererVendor
+)
+
+function rendererJS () {
   return gulp.src(['./src/renderer/**/*.js', '!./src/renderer/**/vendor/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-gulp.task('renderer:vue', () => {
+function rendererVue () {
   return gulp.src('./src/renderer/**/*.vue')
-    .pipe(vueify())
+    .pipe(vueify(babelVue()))
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-gulp.task('renderer:pug', () => {
+function rendererPug () {
   return gulp.src('./src/renderer/**/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-gulp.task('renderer:sass', () => {
+function rendererSass () {
   return gulp.src('./src/renderer/**/*.scss')
     .pipe(sass({ importer }))
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-// Temporary task to simply copy image files
-gulp.task('renderer:assets', () => {
+function rendererAssets () {
   return gulp.src('./src/renderer/**/*.png')
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-gulp.task('renderer:vendor', () => {
+function rendererVendor () {
   return gulp.src('./src/renderer/**/vendor/**/*.js')
     .pipe(gulp.dest('./build/renderer'))
-})
+}
 
-gulp.task('common', ['clean:common'], () => {
+function buildCommon () {
   return gulp.src('./src/common/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./build/common'))
-})
+}
 
-gulp.task('clean:all', () => del('./build'))
-gulp.task('clean:main', () => del('./build/main'))
-gulp.task('clean:renderer', () => del('./build/renderer'))
-gulp.task('clean:common', () => del('./build/common'))
+function watchAll () {
+  gulp.watch('./src/main/**/*.{js,html}', gulp.series(
+    cleanMain, buildMain
+  ))
 
-gulp.task('watch', ['build'], () => {
-  gulp.watch('./src/main/**/*.js', ['clean:main', 'main'])
-  gulp.watch('./src/renderer/**/*.{js,vue,pug,scss}', ['clean:renderer', 'renderer'])
-  gulp.watch('./src/common/**/*.{js,vue,pug}', ['clean:common', 'common'])
-})
+  gulp.watch('./src/common/**/*.{js,vue,pug}', gulp.series(
+    cleanCommon, buildCommon
+  ))
 
-gulp.task('build', ['main', 'renderer', 'common'])
+  gulp.watch('./src/renderer/**/*.{js,vue,pug,scss}', gulp.series(
+    cleanRenderer, buildRenderer
+  ))
+}
+
+export const build = gulp.series(cleanAll, buildMain, buildCommon, buildRenderer)
+export const watch = gulp.series(cleanAll, buildMain, buildCommon, buildRenderer, watchAll)
+
+export default watch
