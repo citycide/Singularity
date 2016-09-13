6d780087ebb2ddb9a980ed5540bbe1d6ad08b82e
citycide <thecitycide@gmail.com>
2016-09-13 03:25:32 -0500

    {refactor} debounce window persistence
    `levers` no longer debounces internally

--- src/main/components/lib/_persistAppState.js
+++ src/main/components/lib/_persistAppState.js
@@ -1,11 +1,23 @@
 import Levers from 'levers'
+import { debounce } from 'lodash'
 import wm from '../WindowManager'
 
 const settings = new Levers('window')
 const mainWindows = wm.getAll('main')
 const mainWindow = mainWindows[0]
 
-const _save = () => {
+const saveTicker = debounce(save, 200, {
+  leading: true,
+  trailing: true,
+  maxWait: 200
+})
+
+mainWindow.on('move', saveTicker)
+mainWindow.on('resize', saveTicker)
+mainWindow.on('maximize', saveTicker)
+mainWindow.on('unmaximize', saveTicker)
+
+function save () {
   if (mainWindow.isMaximized()) {
     settings.set('maximized', true)
   } else {
@@ -14,11 +26,6 @@ const _save = () => {
     settings.set('size', mainWindow.getSize())
   }
 }
-
-mainWindow.on('move', _save)
-mainWindow.on('resize', _save)
-mainWindow.on('maximize', e => _save())
-mainWindow.on('unmaximize', _save)
 
 /*
 Emitter.on('welcomed', (event, version) => {
