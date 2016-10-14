c97aa12c1732eb90e69990299e6b93b7e1aa1a14
citycide <thecitycide@gmail.com>
2016-10-14 13:37:45 -0500

    refactor(bot): forward bot logs to app logger

--- src/main/components/bot/helpers/logger.js
+++ src/main/components/bot/helpers/logger.js
@@ -1,16 +1,26 @@
 import path from 'path'
 import moment from 'moment'
+import { each } from 'lodash'
 import callsites from 'callsites'
+import appLog from 'common/utils/logger'
+
+const logTypes = {
+  error: 0,
+  event: 1,
+  debug: 2,
+  trace: 3
+}
 
 const log = function (file, data) {
-  $.file.write(file, `${moment().format('LTS L')} :: ${data}`, true)
+  const line = `${moment().format('LTS L')} :: ${data}`
+  $.file.write(file, line, true)
+  appLog.bot(line)
 }
 
 log.level = 1
 
 log.clear = function (file) {
   if (!$.file.exists(file)) return
-
   $.file.write(file, '')
 }
 
@@ -20,34 +30,28 @@ log.getLevel = function () {
 
 log.setLevel = function (level) {
   level = parseInt(level)
-  if (typeof level !== 'number') return
+  if (typeof level !== 'number') {
+    level = logTypes[level] || 1
+  }
   log.level = level
 }
 
-const logTypes = {
-  error: 0,
-  event: 1,
-  debug: 2,
-  trace: 3
-}
+Object.keys(logTypes).map(type => {
+  log[type] = function (file, data) {
+    if (log.getLevel() < logTypes[type]) return
 
-;(function () {
-  for (let type of Object.keys(logTypes)) {
-    // eslint-disable-next-line
-    log[type] = function (file, data) {
-      if (log.getLevel() < logTypes[type]) return
+    const traced = callsites()[1]
+    const fileName = path.basename(traced.getFileName())
+    const lineNum = traced.getLineNumber()
+    const colNum = traced.getColumnNumber()
 
-      const traced = callsites()[1]
-      const fileName = path.basename(traced.getFileName())
-      const lineNum = traced.getLineNumber()
-      const colNum = traced.getColumnNumber()
-
-      const outPath = `${type}/${file}`
-      $.file.write(outPath, `${moment()
-      .format('LTS L')} :: ${fileName} (${lineNum}, ${colNum}) -> ${data}`, true)
-    }
+    const outPath = `${type}/${file}`
+    const time = moment().format('LTS L')
+    const line = `${time} :: ${fileName} (${lineNum}, ${colNum}) -> ${data}`
+    $.file.write(outPath, line, true)
+    appLog.bot(line)
   }
-})()
+}
 
 export default log
 
