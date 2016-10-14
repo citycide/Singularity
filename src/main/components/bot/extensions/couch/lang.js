d73c999aee8c4147930fc380a33205a982d65117
citycide <thecitycide@gmail.com>
2016-10-14 13:56:31 -0500

    fix(bot): string quoting typo

--- src/main/components/bot/extensions/couch/lang.js
+++ src/main/components/bot/extensions/couch/lang.js
@@ -1,5 +1,5 @@
 export default function ($) {
-  $.weave.set('did-not-find', 'You didn't find any {0} in the couch this time.')
+  $.weave.set('did-not-find', `You didn't find any {0} in the couch this time.`)
   $.weave.set('found-points', 'You found {0} in the couch!')
   
   $.weave.set('multi.usage', 'Usage: !couch multi (multiplier) » currently set to {0}')
