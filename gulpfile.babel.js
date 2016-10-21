7448f84ce344c6311aa4d93ab945d10fa358ddf2
citycide <thecitycide@gmail.com>
2016-10-21 00:26:23 -0500

    perf(build): speed up build by 500%
    Right, so I don't have exact numbers here but I've sped up the build process IMMENSELY. There were some things being done serially that could be parallel. The watch process was especially slow because it was rebuilding every file even if it hadn't changed.
    
    The build tasks now exclude files that haven't changed, which for example sped up the mainJS task from ~5.15s (every time) to something like 270ms (depending on how many files were edited).

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,39 +1,120 @@
-import del from 'del'
 import gulp from 'gulp'
-import babel from 'gulp-babel'
-import vueify from 'gulp-vueify'
 import sass from 'gulp-sass'
+import babel from 'gulp-babel'
+import newer from 'gulp-newer'
+import vueify from 'gulp-vueify'
+import deleted from 'gulp-deleted'
 import jetpack from 'fs-jetpack'
 import importer from 'node-sass-module-importer'
 
 const babelVue = () => jetpack.read('./.babelrc')
 
-function cleanAll () { return del('./build') }
-function cleanMain () { return del('./build/main') }
-function cleanCommon () { return del('./build/common') }
-function cleanRenderer () { return del('./build/renderer') }
-
-const buildMain = gulp.parallel(mainJS, mainJSON, mainServer)
-
 function mainJS () {
+  const output = './build/main'
   return gulp
     .src('./src/main/**/*.js')
+    .pipe(deleted(output))
+    .pipe(newer(output))
     .pipe(babel())
-    .pipe(gulp.dest('./build/main'))
+    .pipe(gulp.dest(output))
 }
 
 function mainJSON () {
+  const output = './build/main'
   return gulp
     .src('./src/main/**/*.json')
-    .pipe(gulp.dest('./build/main'))
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
 }
 
 function mainServer () {
+  const output = './build/main/components/server/public'
   return gulp
     .src('./src/main/components/server/public/**/*.html')
-    .pipe(gulp.dest('./build/main/components/server/public'))
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
 }
 
+function rendererJS () {
+  const output = './build/renderer'
+  return gulp
+    .src(['./src/renderer/**/*.js', '!./src/renderer/**/vendor/**/*.js'])
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(babel())
+    .pipe(gulp.dest(output))
+}
+
+function rendererVue () {
+  const output = './build/renderer'
+  return gulp
+    .src('./src/renderer/**/*.vue')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(vueify(babelVue()))
+    .pipe(gulp.dest(output))
+}
+
+function rendererSass () {
+  const output = './build/renderer'
+  return gulp
+    .src('./src/renderer/**/*.scss')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(sass({ importer }))
+    .pipe(gulp.dest(output))
+}
+
+function rendererHTML () {
+  const output = './build/renderer'
+  return gulp
+    .src('./src/renderer/**/*.html')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
+}
+
+function rendererAssets () {
+  const output = './build/renderer'
+  return gulp
+    .src('./src/renderer/**/*.png')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
+}
+
+function rendererVendor () {
+  const output = './build/renderer'
+  return gulp
+    .src('./src/renderer/**/vendor/**/*.js')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
+}
+
+function commonJS () {
+  const output = './build/common'
+  return gulp
+    .src('./src/common/**/*.js')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(babel())
+    .pipe(gulp.dest(output))
+}
+
+function commonJSON () {
+  const output = './build/common'
+  return gulp
+    .src('./src/common/**/*.json')
+    .pipe(deleted(output))
+    .pipe(newer(output))
+    .pipe(gulp.dest(output))
+}
+
+const buildMain = gulp.parallel(mainJS, mainJSON, mainServer)
+const buildCommon = gulp.parallel(commonJS, commonJSON)
 const buildRenderer = gulp.parallel(
   rendererJS,
   rendererVue,
@@ -43,75 +124,13 @@ const buildRenderer = gulp.parallel(
   rendererVendor
 )
 
-function rendererJS () {
-  return gulp
-    .src(['./src/renderer/**/*.js', '!./src/renderer/**/vendor/**/*.js'])
-    .pipe(babel())
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-function rendererVue () {
-  return gulp
-    .src('./src/renderer/**/*.vue')
-    .pipe(vueify(babelVue()))
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-function rendererSass () {
-  return gulp
-    .src('./src/renderer/**/*.scss')
-    .pipe(sass({ importer }))
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-function rendererHTML () {
-  return gulp
-    .src('./src/renderer/**/*.html')
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-function rendererAssets () {
-  return gulp
-    .src('./src/renderer/**/*.png')
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-function rendererVendor () {
-  return gulp
-    .src('./src/renderer/**/vendor/**/*.js')
-    .pipe(gulp.dest('./build/renderer'))
-}
-
-const buildCommon = gulp.parallel(commonJS, commonJSON)
-
-function commonJS () {
-  return gulp
-    .src('./src/common/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./build/common'))
-}
-
-function commonJSON () {
-  return gulp
-    .src('./src/common/**/*.json')
-    .pipe(gulp.dest('./build/common'))
-}
-
 function watchAll () {
-  gulp.watch('./src/main/**/*.{js,json,html}', gulp.series(
-    cleanMain, buildMain
-  ))
-
-  gulp.watch('./src/common/**/*.{js,json}', gulp.series(
-    cleanCommon, buildCommon
-  ))
-
-  gulp.watch('./src/renderer/**/*.{js,vue,html,scss}', gulp.series(
-    cleanRenderer, buildRenderer
-  ))
+  gulp.watch('./src/main/**/*.{js,json,html}', buildMain)
+  gulp.watch('./src/common/**/*.{js,json}', buildCommon)
+  gulp.watch('./src/renderer/**/*.{js,vue,html,scss}', buildRenderer)
 }
 
-export const build = gulp.series(cleanAll, buildMain, buildCommon, buildRenderer)
-export const watch = gulp.series(cleanAll, buildMain, buildCommon, buildRenderer, watchAll)
+export const build = gulp.parallel(buildMain, buildCommon, buildRenderer)
+export const watch = gulp.series(build, watchAll)
 
 export default watch
