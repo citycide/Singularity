f0d3e583c3fe63d1443ab861f7552c7183028e8d
citycide <thecitycide@gmail.com>
2016-10-25 10:17:00 -0500

    refactor(bot): simplify ext loader validation
    Clean up the file existence check in the extension loader validation.

--- src/main/components/bot/extension-loader.js
+++ src/main/components/bot/extension-loader.js
@@ -99,12 +99,14 @@ function validate (extPath, manifest) {
     }
   }
 
-  // check that any `files` referenced exist
-  if (!_.every([
+  const file = [
     files.module && fileExists(extDir, files.module),
     files.component && fileExists(extDir, files.component),
     files.interface && fileExists(extDir, files.interface)
-  ].filter(isNilOrEmpty), Boolean)) {
+  ]
+
+  // check that any `files` referenced exist
+  if (!_.every(file.filter(v => !(_.isNil(v) || _.isEmpty(v))), Boolean)) {
     return {
       valid: false,
       reason: `Extension ${name} references files that don't exist. ` +
@@ -186,10 +188,6 @@ function getExtensionPath () {
   // ensure it exists
   dir(directory)
   return directory
-}
-
-function isNilOrEmpty (value) {
-  return _.every([value, !_.isEmpty(value), !_.isNil(value)], Boolean)
 }
 
 function fileExists (extDir, element) {
